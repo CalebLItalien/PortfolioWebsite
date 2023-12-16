5e16c591019e5549962e2855431a56e0b5ca6dbe
Caleb L'Italien <caleblitalien@proton.me>
2023-12-16 08:06:31 -0500

    setup script

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@
 FROM node:14 AS frontend-build
 WORKDIR /app
 COPY ./frontend/package.json ./frontend/package-lock.json ./
+RUN echo "Compiling Frontend"
 RUN npm install
 COPY ./frontend/ ./
 RUN npm run build
@@ -11,11 +12,13 @@ FROM rust:1.73.0 as backend-build
 WORKDIR /usr/src/myapp
 COPY ./api/Cargo.toml ./api/Cargo.lock ./
 COPY ./api/src ./src
+RUN echo "Compiling Backend"
 RUN cargo install --path .
 
 # Finalize Image
 FROM debian:bookworm-slim
 
+RUN echo "Installing Dependencies"
 RUN apt-get update \
     && apt-get install -y --no-install-recommends \
         nginx \
@@ -27,6 +30,7 @@ RUN apt-get update \
         zlib1g-dev \
     && rm -rf /var/lib/apt/lists/*
 
+RUN echo "Linking OpenSSL"
 RUN curl -fsSL https://www.openssl.org/source/openssl-3.0.0.tar.gz -o openssl.tar.gz \
     && tar -zxf openssl.tar.gz \
     && cd openssl-3.0.0 \
@@ -36,6 +40,7 @@ RUN curl -fsSL https://www.openssl.org/source/openssl-3.0.0.tar.gz -o openssl.ta
 
 RUN ldconfig /usr/local/ssl/lib
 
+RUN echo "Serving Static Files"
 COPY ./nginx.conf /etc/nginx/nginx.conf
 
 COPY --from=frontend-build /app/build /var/www/html
@@ -43,6 +48,7 @@ COPY --from=backend-build /usr/local/cargo/bin/resume-website-generator /usr/loc
 
 COPY ./frontend/public/resume.pdf /var/www/html/resume.pdf  
 
-# Port Image
+# Liftoff
+RUN echo "Liftoff"
 EXPOSE 8080 8081
 CMD nginx -g 'daemon off;' && resume-website-generator
--- setup.sh
+++ setup.sh
@@ -1,2 +1,246 @@
 #!/bin/sh
 # Does setup for the site
+
+# Introduction
+echo "\n"
+echo "Hello there! I'm happy you've decided to build a personal website. Please 
+    follow these steps to build your website."
+
+# Requirements
+echo "\n"
+echo "Requirements for the Project:"
+echo "1. Docker - Ensure you have installed the Docker daemon and have it running. 
+    It can be installed at this link: https://www.docker.com/products/docker-desktop/"
+echo "2. Internet connection - To download packages and dependencies."
+echo "3. Your name"
+echo "4. A headshot"
+echo "5. A brief description of yourself"
+echo "6. A GitHub link"
+echo "7. A LinkedIn link"
+echo "8. The names of companies you've worked at (which you want displayed)"
+echo "9. The logos of those companies"
+echo "10. Descriptions of what you did at those companies"
+echo "11. The programming languages you have at least beginner level experience with"
+echo "12. The names of frameworks and libraries you know"
+echo "13. The names of technologies you know"
+echo "14. Icons for the languages, frameworks, and technologies you know, with 
+    transparent backgrounds"
+echo "15. The titles of projects you've worked on"
+echo "16. Links to the projects you've worked on"
+echo "17. Descriptions of the projects you've worked on"
+echo "18. A pdf of your resume"
+echo "19. An SMTP Server name, username, and password - see SMTP.md for more 
+    information"
+echo "20. A no reply email, and a personal email"
+
+# Acknowledge Requirements
+echo "\n"
+while true; do
+    read -p "Phew! that's quite a lot. But it'll all be worth it! Do you have all these? 
+        Y (yes) or N (no): " yn
+    case $yn in 
+        [Yy]* ) break;;
+        [Nn]* ) echo "Please make sure you have all the requirements before proceeding."; exit;;
+        * ) echo "Please answer yes (Y) or no (N)"
+    esac
+done
+
+echo "Great! Let's continue"
+
+# Name
+read -p "Please enter your name: " userName
+echo "$userName" > ./frontend/public/name.txt
+
+# Headshot
+echo "Please open frontend -> src -> assets -> headshot."
+echo "Then, place a 'headshot.png' file there. It must be named exactly 'headshot.png'."
+read -n1 -p "Press any key to continue after you have completed this step."
+
+# Bio
+read -p "Please enter a brief description of yourself" userDescription
+echo  "$userDescription" > ./frontend/public/bio.txt
+
+# GitHub Link
+read -p "Please enter a GitHub link (ie: https://github.com/exampleUser)" gitLink
+echo "$gitLink" > ./frontend/public/github.txt
+
+# LinkedIn Link
+read -p "Please enter a LinkedIn link (ie: https://www.linkedin.com/in/exampleUser/" liLink
+echo "$liLink" > ./frontend/public/linkedin.txt
+
+# Company Names
+echo "Enter the names of the companies one by one. Type '*' and press Enter when you're done."
+while true; do
+    read -p "Enter a company name (or '*' if done):" companyName
+    if ["$companyName" = "*"]; then 
+        break
+    else 
+        echo "$companyName" >> ./frontend/public/companies.txt
+    fi
+done
+
+# Company Logos
+echo "Please open frontend -> src -> assets -> companies."
+echo "Then, for each company you just entered, place a {companyName}.png file as its logo."
+echo "The file name should be all lowercase, with spaces and special characters removed or replaced."
+echo "For example:"
+echo "  - If you worked at 'Example Company AbC LLC.', the file would be 'examplecompanyabcllc.png'"
+echo "  - If you worked at 'My Company, Inc.', the file would be 'mycompanyinc.png'"
+read -n1 -p "Press any key to continue after you have completed this step."
+
+# Company Descriptions
+echo "Enter descriptions of what you did at those companies one by one. Type '*' and press Enter when you're done."
+echo "You must enter the descriptions in the exact same order you entered the names of the companies."
+echo "If you want to view the list of companies you entered, type '*show' and press Enter."
+while true; do
+    read -p "Enter a company description (or '*' if done, '*show' to view companies): " companyDescription
+    if [ "$companyDescription" = "*" ]; then
+        break
+    elif [ "$companyDescription" = "show" ]; then
+        echo "List of Companies:"
+        cat ./frontend/public/companies.txt
+        echo "\nContinue entering company descriptions."
+    else
+        echo "$companyDescription" >> ./frontend/public/companyDescriptions.txt
+    fi
+done
+
+# Programming Language
+echo "Enter the names of programming languages you have at least beginnner level experience with."
+echo "For each language, first, type its name, press Enter, and then enter your expertise level (B/I/A)."
+while true; do
+    read -p "Enter a programming language (or '*' if finished):" language
+    if [ "$language" = "*" ]; then
+        break
+    else
+        read -p "Enter your expertise level, as B (for Beginner), I (for Intermediate) or A(for Advanced): " expertise
+        case "$expertise" in
+            B|I|A)
+                case "$expertise" in 
+                    B) expertise="Beginner" ;;
+                    I) expertise="Intermediate" ;;
+                    A) expertise="Advanced" ;;
+                esac
+                echo "$language $expertise" >> ./frontend/public/languages.txt
+                ;;
+            *)
+                echo "Invalid expertise level. Please enter B, I, or A."
+                ;;
+        esac
+    fi
+done
+
+# Frameworks and Libraries
+echo "Enter the names of frameworks and libraries you have at least beginnner level experience with."
+echo "For each tool, first, type its name, press Enter, and then enter your expertise level (B/I/A)."
+while true; do
+    read -p "Enter a tool (or '*' if finished):" tool
+    if [ "$tool" = "*" ]; then
+        break
+    else
+        read -p "Enter your expertise level, as B (for Beginner), I (for Intermediate) or A(for Advanced): " expertise
+        case "$expertise" in
+            B|I|A)
+                case "$expertise" in 
+                    B) expertise="Beginner" ;;
+                    I) expertise="Intermediate" ;;
+                    A) expertise="Advanced" ;;
+                esac
+                echo "$tool $expertise" >> ./frontend/public/frameworksLibraries.txt
+                ;;
+            *)
+                echo "Invalid expertise level. Please enter B, I, or A."
+                ;;
+        esac
+    fi
+done
+
+# Technologies 
+echo "Enter the names of technologies you have at least beginnner level experience with."
+echo "For each technology, first, type its name, press Enter, and then enter your expertise level (B/I/A)."
+while true; do
+    read -p "Enter a technology (or '*' if finished):" technology
+    if [ "$technology" = "*" ]; then
+        breaks
+    else
+        read -p "Enter your expertise level, as B (for Beginner), I (for Intermediate) or A(for Advanced): " expertise
+        case "$expertise" in
+            B|I|A)
+                case "$expertise" in 
+                    B) expertise="Beginner" ;;
+                    I) expertise="Intermediate" ;;
+                    A) expertise="Advanced" ;;
+                esac
+                echo "$technology $expertise" >> ./frontend/public/technologies.txt
+                ;;
+            *)
+                echo "Invalid expertise level. Please enter B, I, or A."
+                ;;
+        esac
+    fi
+done
+
+# Technology Icons
+
+# Project Titles
+
+# Project Links
+
+# Project Descriptions
+
+# PDF of Resume
+echo "Please open frontend -> public."
+echo "Then, place a 'resume.pdf' file there. It must be named exactly 'resume.pdf'."
+read -n1 -p "Press any key to continue after you have completed this step."
+
+# SMTP Setup
+ENV_PATH="./api/.env"
+read -p "Please enter your SMTP Server name: " smtpServer
+KEY="SMTP_SERVER"
+VALUE="$smtpServer"
+
+if grep -q "^KEY=" "$ENV_PATH"; then
+    sed -i "s/^KEY=.*/$KEY=$VALUE/" "$ENV_PATH"
+else
+    echo "$KEY=$VALUE" >> "$ENV_PATH"
+fi
+
+read -p "Please enter your SMTP Username: " smtpUser
+KEY="SMTP_USERNAME"
+VALUE="$smtpUser"
+
+if grep -q "^KEY=" "$ENV_PATH"; then
+    set -i "s/^KEY=.*/$KEY=$VALUE/" "$ENV_PATH"
+else
+    echo "$KEY=$VALUE" >> "$ENV_PATH"
+fi
+
+read -p "Please enter your SMTP Password: " smtpPassword
+KEY="SMTP_PASSWORD"
+VALUE="$smtpPassword"
+
+if grep -q "^KEY=" "$ENV_PATH"; then
+    sed -i "s/^KEY=.*/$KEY=$VALUE/" "$ENV_PATH"
+else
+    echo "$KEY=$VALUE" >> "$ENV_PATH"
+fi
+
+read -p "Please enter your no reply email: " noReply
+KEY="NO_REPLY_EMAIL"
+VALUE="$noReply"
+
+if grep -q "^KEY=" "$ENV_PATH"; then
+    sed -i "s/^KEY=.*/$KEY=$VALUE/" "$ENV_PATH"
+else
+    echo "$KEY=$VALUE" >> "$ENV_PATH"
+fi
+
+read -p "Please enter your inbox email: " inbox
+KEY="INBOX_EMAIL"
+VALUE="$inbox"
+
+if grep -q "^KEY=" "$ENV_PATH"; then 
+    sed -i "s/^KEY=.*/$KEY=$VALUE/" "$ENV_PATH"
+else    
+    echo "$KEY=$VALUE" >> "$ENV_PATH"
+fi
